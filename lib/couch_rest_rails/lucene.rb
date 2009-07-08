2c4a8a733e6bc5500aa736309c24c77b4d281ceb
Arnaud Berthomier <arnaud@cyprio.net>
2009-07-08 16:53:10 +0200

    get a little less if-else-y

--- lib/couch_rest_rails/lucene.rb
+++ lib/couch_rest_rails/lucene.rb
@@ -1,56 +1,52 @@
 module CouchRestRails
   module Lucene
-
     extend self
 
+    # Push index/search views to CouchDB
     def push(database, design_doc)
+      puts "database = #{database} :: design_doc = #{design_doc}"
       result = []
-      puts "database = #{database} :: design_doc=#{design_doc}"
 
-      return  "Database '#{database}' doesn't exists" unless (database == "*" ||
-                                                              File.exist?(File.join(RAILS_ROOT,
-                                                                                    CouchRestRails.setup_path,
-                                                                                    database)))
-      Dir[File.join(RAILS_ROOT, CouchRestRails.setup_path, database)].each do |db|
-        # check for a directory...
-        if File::directory?(db)
-          if File.exist?("#{db}/lucene")
-            db_name =COUCHDB_CONFIG[:db_prefix] +  File.basename( db) +
-              COUCHDB_CONFIG[:db_suffix]
-            res = CouchRest.get("#{COUCHDB_CONFIG[:host_path]}/#{db_name}") rescue nil
-            if res
-              db_con = CouchRest.database("#{COUCHDB_CONFIG[:host_path]}/#{db_name}")
-              doc = File.join(db,"lucene")
-              design_doc_name = File.basename(doc)
-              search = assemble_lucene(doc)
-              if search.blank?
-                result << "No search was found in #{doc}/#{File.basename(doc)}"
-              else
-                db_con.save_doc({ "_id" => "_design/#{design_doc_name}",
-                                  :fulltext => search
-                                })
-                result << "Added lucene-search the following design_doc to _design/#{design_doc_name}: #{search.map {|k,v| k.to_s}.join(', ')}"
-              end
-            else
-              return  "CouchDB database '#{db_name}' doesn't exist. create it first"
-            end
-          else
-            puts "lucene directory (#{db}/lucence) does not exist"
-          end
+      db_dir = File.join(RAILS_ROOT, CouchRestRails.setup_path, database)
+      return "Database '#{database}' does not exist" unless (database == "*" || File.exist?(db_dir))
+
+      Dir[db_dir].each do |db|
+        return "Lucene directory '#{db}/lucene' does not exist." unless File.exist?("#{db}/lucene")
+
+        # CouchDB checks
+        db_name = COUCHDB_CONFIG[:db_prefix] + File.basename(db) + COUCHDB_CONFIG[:db_suffix]
+        begin
+          CouchRest.get("#{COUCHDB_CONFIG[:host_path]}/#{db_name}")
+        rescue => err
+          return "CouchDB database '#{db_name}' does not exist. Create it first. (#{err})"
+        end
+
+        # Load lucene views from FS
+        doc_path = File.join(db, 'lucene')
+        index_views = assemble_lucene(doc_path)
+
+        # Update CouchDB designs
+        db_con = CouchRest.database("#{COUCHDB_CONFIG[:host_path]}/#{db_name}")
+        if index_views.blank?
+          result << "No search was found in #{doc_path}/#{File.basename(doc_path)}"
         else
-          return "CouchDB database '#{database}' doesn't exist"
+          design_id = "_design/#{File.basename(doc_path)}"
+          db_con.save_doc({ "_id" => design_id,
+                            :fulltext => index_views
+                          })
+          result << "Added lucene-search the following design_doc to #{design_id}: #{index_views.keys.join(', ')}"
         end
       end
       result.join("\n")
     end
 
+    # Assemble views from file-system path lucene_doc_path
     def assemble_lucene(lucene_doc_path)
       search = {}
       if File.exist?(File.join(lucene_doc_path, 'lucene.js'))
-        defaults = {"store" => "no", "index"=>"not_analyzed"}
-        lucene = {"defaults" =>  defaults}
-        lucene["index"] = IO.read(File.join(lucene_doc_path, 'lucene.js')).gsub(/\n/,"")
-        search["message_search"] = lucene
+        lucene = {'defaults' => { 'store' => 'no', 'index' => 'not_analyzed' } }
+        lucene['index'] = IO.read(File.join(lucene_doc_path, 'lucene.js')).gsub(/\n/, '')
+        search['message_search'] = lucene
       end
       search
     end
--- lib/couch_rest_rails/views.rb
+++ lib/couch_rest_rails/views.rb
@@ -5,48 +5,48 @@ module CouchRestRails
     # Push views to couchdb
     def push(database, design_doc)
       puts "database = #{database} :: design_doc = #{design_doc}"
-      return  "Database '#{database}' doesn't exists" unless (database == "*" || File.exist?(File.join(RAILS_ROOT, CouchRestRails.setup_path, database)))
+      result = []
 
-      Dir[File.join(RAILS_ROOT, CouchRestRails.setup_path, database)].each do |db|
-        result = []
+      db_dir = File.join(RAILS_ROOT, CouchRestRails.setup_path, database)
+      return "Database directory '#{database}' does not exist" unless (database == "*" || File.exist?(db_dir))
 
-        # Check for a directory...
-        if File.directory?(db)
-          return "Views directory (#{db}/views) does not exist" unless File.exist?("#{db}/views")
-          db_name = COUCHDB_CONFIG[:db_prefix] +  File.basename(db) + COUCHDB_CONFIG[:db_suffix]
-          res = CouchRest.get("#{COUCHDB_CONFIG[:host_path]}/#{db_name}") rescue nil
+      Dir[db_dir].each do |db|
+        return "Views directory '#{db}/views' does not exist"  unless File.exist?("#{db}/views")
 
-          if res
-            db_con = CouchRest.database("#{COUCHDB_CONFIG[:host_path]}/#{db_name}")
-            Dir.glob(File.join(db, "views", design_doc)).each do |doc|
-              design_doc_name = File.basename(doc)
-
-              # Load views from filesystem & from couchdb
-              views = assemble_views(doc)
-              couchdb_design = db_con.get("_design/#{design_doc_name}") rescue nil
-
-              # Update couchdb existing views...
-              views = couchdb_design['views'].merge(views) unless couchdb_design.nil?
-
-              if views.empty?
-                result << "No updatable views in #{doc}/#{File.basename(doc)}"
-              else
-                rev = couchdb_design.nil? ? {} : { '_rev' => couchdb_design['_rev'] }
-                db_con.save_doc({
-                                  '_id' => "_design/#{design_doc_name}",
-                                  'language' => 'javascript',
-                                  'views' => views
-                                }.merge(rev))
-                result << "Added views to _design/#{design_doc_name}: #{views.map {|k,v| k }.join(', ')}"
-              end
-            end
-            puts "No views were found in '#{File.join(db, "views", design_doc)}'" if result.empty?
-          else
-            return  "CouchDB database '#{db_name}' doesn't exist! Create it first."
-          end
-        else
-          return "CouchDB database '#{database}' doesn't exist"
+        # CouchDB checks
+        db_name = COUCHDB_CONFIG[:db_prefix] + File.basename(db) + COUCHDB_CONFIG[:db_suffix]
+        begin
+          CouchRest.get("#{COUCHDB_CONFIG[:host_path]}/#{db_name}")
+        rescue => err
+          return "CouchDB database '#{db_name}' does not exist. Create it first. (#{err})"
         end
+
+        db_con = CouchRest.database("#{COUCHDB_CONFIG[:host_path]}/#{db_name}")
+        Dir.glob(File.join(db, "views", design_doc)).each do |doc|
+          design_doc_name = File.basename(doc)
+
+          # Load views from filesystem & couchdb
+          views = assemble_views(doc)
+          couchdb_design = db_con.get("_design/#{design_doc_name}") rescue nil
+
+          # Update couchdb's freshly-fetched views...
+          views = couchdb_design['views'].merge(views) unless couchdb_design.nil?
+
+          if views.empty?
+            result << "No updatable views in #{doc}/#{File.basename(doc)}"
+          else
+            rev = couchdb_design.nil? ? {} : { '_rev' => couchdb_design['_rev'] }
+            id  = "_design/#{design_doc_name}"
+            db_con.save_doc({
+                              '_id'      => id,
+                              'language' => 'javascript',
+                              'views'    => views
+                            }.merge(rev))
+            result << "Added views to #{id}: #{views.keys.join(', ')}"
+          end
+        end
+
+        return "No views were found in '#{File.join(db, "views", design_doc)}'" if result.empty?
         return "#{result.join("\n")}"
       end
     end
