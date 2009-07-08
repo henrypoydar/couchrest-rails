f3736d23a3bf74448a49e4972fe7a6024065df25
dave farkas <dave@interactivemediums.com>
2009-07-08 15:05:18 -0500

    allow creating and deleting specific databases

--- lib/couch_rest_rails/database.rb
+++ lib/couch_rest_rails/database.rb
@@ -3,11 +3,16 @@ module CouchRestRails
 
     extend self
 
-    def create
+    def create(database)
+      return  "Database '#{database}' doesn't exists" unless
+        (database == "*" || File.exist?(File.join(RAILS_ROOT,
+                                                  CouchRestRails.setup_path,
+                                                  database)))
+
       # get list of available_databases in couch...
       existing_databases = COUCHDB_SERVER.databases
       # get all the model files
-      Dir[File.join(RAILS_ROOT, CouchRestRails.setup_path,'*')].each do |db|
+      Dir[File.join(RAILS_ROOT, CouchRestRails.setup_path,database)].each do |db|
         # check for a directory...
         if File::directory?( db )
           database_name =COUCHDB_CONFIG[:db_prefix] +  File.basename(db) +
@@ -28,11 +33,15 @@ module CouchRestRails
       "create complete"
     end
 
-    def delete
+    def delete(database)
+      return  "Database '#{database}' doesn't exists" unless
+        (database == "*" || File.exist?(File.join(RAILS_ROOT,
+                                                  CouchRestRails.setup_path,
+                                                  database)))
       # get list of available_databases in couch...
       existing_databases = COUCHDB_SERVER.databases
       # get all the model files
-      Dir[File.join(RAILS_ROOT, CouchRestRails.setup_path,"*")].each do |db|
+      Dir[File.join(RAILS_ROOT, CouchRestRails.setup_path,database)].each do |db|
         # check for a directory...
         if File::directory?( db )
           database_name =COUCHDB_CONFIG[:db_prefix] +  File.basename(db) +
