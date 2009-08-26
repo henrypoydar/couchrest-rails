53b00c7e716f686032a7df03712f64e9b0d30c94
hpoydar <hpoydar@gmail.com>
2009-08-26 12:16:17 -0400

    Fixed document spec

--- spec/lib/couch_rest_rails/document_spec.rb
+++ spec/lib/couch_rest_rails/document_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 describe CouchRestRails::Document do
   
   class CouchRestRailsTestDocument < CouchRestRails::Document 
-    
+    use_database :foo
   end
   
   before :each do
@@ -14,8 +14,8 @@ describe CouchRestRails::Document do
     CouchRestRails::Document.ancestors.include?(CouchRest::ExtendedDocument).should be_true
   end
   
-  it "should use the COUCHDB_SERVER constant to define its CouchDB connection" do
-    @doc.database.name.should == COUCHDB_CONFIG[:database]
+  it "should define its CouchDB connection and CouchDB database name" do
+    @doc.database.name.should == "#{COUCHDB_CONFIG[:db_prefix]}foo#{COUCHDB_CONFIG[:db_suffix]}"
   end
 
   
